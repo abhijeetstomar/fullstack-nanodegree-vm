d3c35e49b0be29409fea9041ed70c234237e7083
cseas <contact.abhijeetsingh@gmail.com>
2018-08-19 01:02:11 +0530

    Protection against script injection attacks

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,6 +1,7 @@
 # "Database code" for the DB Forum.
 
 import psycopg2
+import bleach
 
 DBNAME = "forum"
 
@@ -17,10 +18,14 @@ def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
+  # sanitize input with bleach to avoid script injection attack
+  content = bleach.clean(content)
   # We substitute %s with a python tuple (hence the comma) to avoid
   # SQL injection attacks.
   c.execute("insert into posts values (%s)", (content,))
+  # Code to replace spam with a message
+  # % means any string can go there
+  # c.execute(
+  #   "update posts set content = 'This spam message has been deleted!' where content like '%Spam%'")
   db.commit()
   db.close()
-
-
